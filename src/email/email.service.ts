62f1bdbd4d3c6d3efcfe6f1e65eb81cb5b5291db
shalev ben hayun <shalev.ben.hayun@agamimp.net>
2024-06-19 13:39:37 +0300

    fix rtl email body

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -63,89 +63,93 @@ export class EmailService {
       address,
       cameras,
       emailAddress,
-      managerOption
+      managerOption,
     } = email;
 
-    let htmlContent = '';
-
-    if (managerOption === 'מנהל') {
-      htmlContent = `
-      <b>תאריך:</b> ${date}<br>
-      <b>רשת:</b> ${company}<br>
-      <b>כתובת הסניף:</b> ${address}<br>
-      <b>שם ה${managerOption}:</b> ${manager}<br>
-      <b>עובד 1:</b> ${employee1 ? employee1 : 'לא קיים'}<br>
-      <b>עובד 2:</b> ${employee2 ? employee2 : 'לא קיים'}<br>
-      <b>עובד 3:</b> ${employee3 ? employee3 : 'לא קיים'}<br>
-      <b>עובד 4:</b> ${employee4 ? employee4 : 'לא קיים'}<br><br>
-      <b>:כספת</b><br>
-      <b>כספת תקנית:</b> ${safe ? 'כן' : 'לא'}<br>
-      <b>מפתחות כספת בידי מנהל/זכיין:</b> ${keys ? 'כן' : 'לא'}<br>
-      <b>רישום תקין על גבי מעטפות הפקדה:</b> ${envelopes ? 'כן' : 'לא'}<br>
-      <b>דוח תואם לסכום במעטפה במזומן:</b> ${cash ? 'כן' : 'לא'}<br>
-      <b>קלסר הפקדות מודיעין אזרחי תואם:</b> ${folder ? 'כן' : 'לא'}<br>
-      <b>עד 6 מעטפות יומיות בכספת:</b> ${envelopesInDay ? 'כן' : 'לא'}<br>
-      <b>מצלמות:</b> ${cameras ? 'כן' : 'לא'}<br>
-      <b>הערות כספת:</b> ${safeNotes ? safeNotes : "אין"}<br><br>
-      <b>:קופה</b><br>
-      <b>קרן קופה תקין:</b> ${register ? 'כן' : 'לא'}<br>
-      <b>דוח תואם לפדיון בקופה:</b> ${redemption ? 'כן' : 'לא'}<br>
-      <b>בדיקת ביטול מזומן בקופה:</b> ${cashCancellation ? 'כן' : 'לא'}<br>
-      <b>בדיקת זיכויים בסכומים חריגים:</b> ${unusualAmounts ? 'כן' : 'לא'}<br>
-      <b>בדיקת שימוש בכרטיס "חבר מועדון":</b> ${clubMember ? 'כן' : 'לא'}<br>
-      <b>בדיקת קופה קטנה (רשת):</b> ${smallRegister ? 'כן' : 'לא'}<br>
-      <b>הערות קופה:</b> ${registerNotes ? registerNotes : "אין"}<br><br>
-      <b>:ניהול מלאי</b><br>
-      <b>תעודות פתוחות "נפרק בחנות" עד 5 ימים:</b> ${unloadedDocuments ? 'כן' : 'לא'}<br>
-      <b>תעודות פתוחות אחרות עד 5 ימים:</b> ${otherDocuments ? 'כן' : 'לא'}<br>
-      <b>ביקורת מלאי מדגמית מהמחסן:</b> ${warehouseStock ? 'כן' : 'לא'}<br>
-      <b>הערות מלאי:</b> ${stockNotes ? stockNotes : "אין"}<br><br>
-      <b>:נוכחות עובדים</b><br>
-      <b>נוכחות עובדים בסניף:</b> ${employeeAttendance ? 'כן' : 'לא'}<br>
-      <b>הערות נוכחות עובדים:</b> ${attendanceNotes ? attendanceNotes : "אין"}<br><br>
-      <b>:תשאול עובדים</b><br>
-      <b>הערות תשאול עובדים:</b> ${employeeNotes ? employeeNotes : "אין"}<br>
-      <br><br>
-      <b>ציון:</b> ${getGrade(email)}
+    const htmlContent = `
+      <html>
+      <body style="direction: rtl; text-align: right;">
+        ${
+          managerOption === 'מנהל'
+            ? `
+          <b>תאריך:</b> ${date}<br>
+          <b>רשת:</b> ${company}<br>
+          <b>כתובת הסניף:</b> ${address}<br>
+          <b>שם ה${managerOption}:</b> ${manager}<br>
+          <b>עובד 1:</b> ${employee1 ? employee1 : 'לא קיים'}<br>
+          <b>עובד 2:</b> ${employee2 ? employee2 : 'לא קיים'}<br>
+          <b>עובד 3:</b> ${employee3 ? employee3 : 'לא קיים'}<br>
+          <b>עובד 4:</b> ${employee4 ? employee4 : 'לא קיים'}<br><br>
+          <b>:כספת</b><br>
+          <b>כספת תקנית:</b> ${safe ? 'כן' : 'לא'}<br>
+          <b>מפתחות כספת בידי מנהל/זכיין:</b> ${keys ? 'כן' : 'לא'}<br>
+          <b>רישום תקין על גבי מעטפות הפקדה:</b> ${envelopes ? 'כן' : 'לא'}<br>
+          <b>דוח תואם לסכום במעטפה במזומן:</b> ${cash ? 'כן' : 'לא'}<br>
+          <b>קלסר הפקדות מודיעין אזרחי תואם:</b> ${folder ? 'כן' : 'לא'}<br>
+          <b>עד 6 מעטפות יומיות בכספת:</b> ${envelopesInDay ? 'כן' : 'לא'}<br>
+          <b>מצלמות:</b> ${cameras ? 'כן' : 'לא'}<br>
+          <b>הערות כספת:</b> ${safeNotes ? safeNotes : 'אין'}<br><br>
+          <b>:קופה</b><br>
+          <b>קרן קופה תקין:</b> ${register ? 'כן' : 'לא'}<br>
+          <b>דוח תואם לפדיון בקופה:</b> ${redemption ? 'כן' : 'לא'}<br>
+          <b>בדיקת ביטול מזומן בקופה:</b> ${cashCancellation ? 'כן' : 'לא'}<br>
+          <b>בדיקת זיכויים בסכומים חריגים:</b> ${unusualAmounts ? 'כן' : 'לא'}<br>
+          <b>בדיקת שימוש בכרטיס "חבר מועדון":</b> ${clubMember ? 'כן' : 'לא'}<br>
+          <b>בדיקת קופה קטנה (רשת):</b> ${smallRegister ? 'כן' : 'לא'}<br>
+          <b>הערות קופה:</b> ${registerNotes ? registerNotes : 'אין'}<br><br>
+          <b>:ניהול מלאי</b><br>
+          <b>תעודות פתוחות "נפרק בחנות" עד 5 ימים:</b> ${unloadedDocuments ? 'כן' : 'לא'}<br>
+          <b>תעודות פתוחות אחרות עד 5 ימים:</b> ${otherDocuments ? 'כן' : 'לא'}<br>
+          <b>ביקורת מלאי מדגמית מהמחסן:</b> ${warehouseStock ? 'כן' : 'לא'}<br>
+          <b>הערות מלאי:</b> ${stockNotes ? stockNotes : 'אין'}<br><br>
+          <b>:נוכחות עובדים</b><br>
+          <b>נוכחות עובדים בסניף:</b> ${employeeAttendance ? 'כן' : 'לא'}<br>
+          <b>הערות נוכחות עובדים:</b> ${attendanceNotes ? attendanceNotes : 'אין'}<br><br>
+          <b>:תשאול עובדים</b><br>
+          <b>הערות תשאול עובדים:</b> ${employeeNotes ? employeeNotes : 'אין'}<br>
+          <br><br>
+          <b>ציון:</b> ${getGrade(email)}
+        `
+            : `
+          <b>תאריך:</b> ${date}<br>
+          <b>רשת:</b> ${company}<br>
+          <b>כתובת הסניף:</b> ${address}<br>
+          <b>שם ה${managerOption}:</b> ${manager}<br>
+          <b>עובד 1:</b> ${employee1 ? employee1 : 'לא קיים'}<br>
+          <b>עובד 2:</b> ${employee2 ? employee2 : 'לא קיים'}<br>
+          <b>עובד 3:</b> ${employee3 ? employee3 : 'לא קיים'}<br>
+          <b>עובד 4:</b> ${employee4 ? employee4 : 'לא קיים'}<br><br>
+          <b>:כספת</b><br>
+          <b>כספת תקנית:</b> ${safe ? 'כן' : 'לא'}<br>
+          <b>מפתחות כספת בידי מנהל/זכיין:</b> ${keys ? 'כן' : 'לא'}<br>
+          <b>רישום תקין על גבי מעטפות הפקדה:</b> ${envelopes ? 'כן' : 'לא'}<br>
+          <b>דוח תואם לסכום במעטפה במזומן:</b> ${cash ? 'כן' : 'לא'}<br>
+          <b>קלסר הפקדות מודיעין אזרחי תואם:</b> ${folder ? 'כן' : 'לא'}<br>
+          <b>עד 6 מעטפות יומיות בכספת:</b> ${envelopesInDay ? 'כן' : 'לא'}<br>
+          <b>מצלמות:</b> ${cameras ? 'כן' : 'לא'}<br>
+          <b>הערות כספת:</b> ${safeNotes ? safeNotes : 'אין'}<br><br>
+          <b>:קופה</b><br>
+          <b>קרן קופה תקין:</b> ${register ? 'כן' : 'לא'}<br>
+          <b>דוח תואם לפדיון בקופה:</b> ${redemption ? 'כן' : 'לא'}<br>
+          <b>בדיקת ביטול מזומן בקופה:</b> ${cashCancellation ? 'כן' : 'לא'}<br>
+          <b>בדיקת זיכויים בסכומים חריגים:</b> ${unusualAmounts ? 'כן' : 'לא'}<br>
+          <b>בדיקת שימוש בכרטיס "חבר מועדון":</b> ${clubMember ? 'כן' : 'לא'}<br>
+          <b>הערות קופה:</b> ${registerNotes ? registerNotes : 'אין'}<br><br>
+          <b>:ניהול מלאי</b><br>
+          <b>תעודות פתוחות "נפרק בחנות" עד 5 ימים:</b> ${unloadedDocuments ? 'כן' : 'לא'}<br>
+          <b>תעודות פתוחות אחרות עד 5 ימים:</b> ${otherDocuments ? 'כן' : 'לא'}<br>
+          <b>ביקורת מלאי מדגמית מהמחסן:</b> ${warehouseStock ? 'כן' : 'לא'}<br>
+          <b>הערות מלאי:</b> ${stockNotes ? stockNotes : 'אין'}<br><br>
+          <b>:תשאול עובדים</b><br>
+          <b>הערות תשאול עובדים:</b> ${employeeNotes ? employeeNotes : 'אין'}<br>
+          <br><br>
+          <b>ציון:</b> ${getGrade(email)}
+        `
+        }
+      </body>
+      </html>
     `;
-    } else {
-      htmlContent = `
-        <b>תאריך:</b> ${date}<br>
-        <b>רשת:</b> ${company}<br>
-        <b>כתובת הסניף:</b> ${address}<br>
-        <b>שם ה${managerOption}:</b> ${manager}<br>
-        <b>עובד 1:</b> ${employee1 ? employee1 : 'לא קיים'}<br>
-        <b>עובד 2:</b> ${employee2 ? employee2 : 'לא קיים'}<br>
-        <b>עובד 3:</b> ${employee3 ? employee3 : 'לא קיים'}<br>
-        <b>עובד 4:</b> ${employee4 ? employee4 : 'לא קיים'}<br><br>
-        <b>:כספת</b><br>
-        <b>כספת תקנית:</b> ${safe ? 'כן' : 'לא'}<br>
-        <b>מפתחות כספת בידי מנהל/זכיין:</b> ${keys ? 'כן' : 'לא'}<br>
-        <b>רישום תקין על גבי מעטפות הפקדה:</b> ${envelopes ? 'כן' : 'לא'}<br>
-        <b>דוח תואם לסכום במעטפה במזומן:</b> ${cash ? 'כן' : 'לא'}<br>
-        <b>קלסר הפקדות מודיעין אזרחי תואם:</b> ${folder ? 'כן' : 'לא'}<br>
-        <b>עד 6 מעטפות יומיות בכספת:</b> ${envelopesInDay ? 'כן' : 'לא'}<br>
-        <b>מצלמות:</b> ${cameras ? 'כן' : 'לא'}<br>
-        <b>הערות כספת:</b> ${safeNotes ? safeNotes : "אין"}<br><br>
-        <b>:קופה</b><br>
-        <b>קרן קופה תקין:</b> ${register ? 'כן' : 'לא'}<br>
-        <b>דוח תואם לפדיון בקופה:</b> ${redemption ? 'כן' : 'לא'}<br>
-        <b>בדיקת ביטול מזומן בקופה:</b> ${cashCancellation ? 'כן' : 'לא'}<br>
-        <b>בדיקת זיכויים בסכומים חריגים:</b> ${unusualAmounts ? 'כן' : 'לא'}<br>
-        <b>בדיקת שימוש בכרטיס "חבר מועדון":</b> ${clubMember ? 'כן' : 'לא'}<br>
-        <b>הערות קופה:</b> ${registerNotes ? registerNotes : "אין"}<br><br>
-        <b>:ניהול מלאי</b><br>
-        <b>תעודות פתוחות "נפרק בחנות" עד 5 ימים:</b> ${unloadedDocuments ? 'כן' : 'לא'}<br>
-        <b>תעודות פתוחות אחרות עד 5 ימים:</b> ${otherDocuments ? 'כן' : 'לא'}<br>
-        <b>ביקורת מלאי מדגמית מהמחסן:</b> ${warehouseStock ? 'כן' : 'לא'}<br>
-        <b>הערות מלאי:</b> ${stockNotes ? stockNotes : "אין"}<br><br>
-        <b>:תשאול עובדים</b><br>
-        <b>הערות תשאול עובדים:</b> ${employeeNotes ? employeeNotes : "אין"}<br>
-        <br><br>
-        <b>ציון:</b> ${getGrade(email)}
-      `;
-    }
-// Join the array elements into a single string separated by commas
+    // Join the array elements into a single string separated by commas
     // const emails = emailAddress.map((email)=>`${email},`)
     // Define email options
     const mailOptions = {
